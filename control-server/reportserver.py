0e0861148b4a1c4178d540bc8e969e75cef6e18f
Jonathan Ludwig <jr.ludwig@gmail.com>
2020-12-20 17:01:11 +0000

    Bug fixes and enhancements

--- control-server/reportserver.py
+++ control-server/reportserver.py
@@ -22,15 +22,15 @@ class ReportServer(object):
         #                           'humidity': random.randint(38, 42)
         #                         })
         map = {}
-        db = StationDatabase()
-        for stationId, station in db.stations.items():
-            map[stationId] = []
-            for dataPoint in station.dataPoints:
-                dataPoint['time'] = int(time.mktime(dataPoint['time'].timetuple()))
-                if stationId == 1:  # Vent monitor
-                    if dataPoint['temperature'] > 77:   # 5 degrees higher than the thermostat would ever be set to for heating
-                        dataPoint['temperature'] = 77
-                    elif dataPoint['temperature'] < 60: # 5 degrees lower than the thermostat would ever be set to for cooling
-                        dataPoint['temperature'] = 60
-                map[stationId].append(dataPoint)
+        with StationDatabase() as db:
+            for stationId, station in db.stations.items():
+                map[stationId] = []
+                for dataPoint in station.dataPoints:
+                    dataPoint['time'] = int(time.mktime(dataPoint['time'].timetuple()))
+                    if stationId == 1:  # Vent monitor
+                        if dataPoint['temperature'] > 77:   # 5 degrees higher than the thermostat would ever be set to for heating
+                            dataPoint['temperature'] = 77
+                        elif dataPoint['temperature'] < 60: # 5 degrees lower than the thermostat would ever be set to for cooling
+                            dataPoint['temperature'] = 60
+                    map[stationId].append(dataPoint)
         return json.dumps(map)
