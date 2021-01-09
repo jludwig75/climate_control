28aed81b7be8feea9963572f22e9bb4193c62364
Jonathan Ludwig <jr.ludwig@gmail.com>
2021-01-09 15:24:24 -0700

    Added HVAC controller to topic conversations

--- control-server/climate/topics.py
+++ control-server/climate/topics.py
@@ -7,21 +7,32 @@ STATION_MSG_TYPE_UPDATE_STATE='updateState'
 STATION_MSG_TYPE_WAIT_FOR_UPDATE='waitForUpdate'
 
 """
-Conversation:
+Conversations
+=============
+Note: broker is omitted where not necessary to be show
 
-STATION UPDATE:
+Sensor Update
+-------------
+    station client --> "climate/station/<id>/sensorData", "{"temperature": float, "humidity": int, "vcc": float}" --> update client
 
+Station Update
+--------------
     On normal run when starting up after sending temperature (as is works)
-    station client --- "climate/station/<id>/updateState", "NotWaiting" --> broker --> update client
+    station client --> "climate/station/<id>/updateState", "NotWaiting" --> update client
 
     station client <-- "climage/<id>/waitForUpdate", "yes" <-- update client
     station client initialzies OTA
-    station client --- "climate/station/<id>/updateState", "Waiting" --> update client
+    station client --> "climate/station/<id>/updateState", "Waiting" --> update client
 
     station client <-- OTA start <-- PlatformIO
-    station client --- "climate/station/<id>/updateState", "Updating" --> update client
+    station client --> "climate/station/<id>/updateState", "Updating" --> update client
     station client <-- OTA end <-- PlatformIO
-    station client --- "climate/station/<id>/updateState", "UpdateComplete" --> update client
+    station client --> "climate/station/<id>/updateState", "UpdateComplete" --> update client
+
+HVAC Controller
+------------------
+    control server --> "climate/hvac/<id>/requestMode", "Off, Fan, Heat, Cool" --> HVAC controller
+    control server <-- "climate/hvac/<id>/currentMode", "Off, Fan, Heat, Cool" <-- HVAC controller
 
 """
 
