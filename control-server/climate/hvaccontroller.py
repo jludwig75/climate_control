14df7d3cc46ff95ce13e58f165dc641930e0b58c
Jonathan Ludwig <jr.ludwig@gmail.com>
2021-01-15 10:45:36 -0700

    Fixed memory leak with timer

--- control-server/climate/hvaccontroller.py
+++ control-server/climate/hvaccontroller.py
@@ -23,6 +23,8 @@ class HvacController(ClimateMqttClient):
         self._initialized = False
         self._pendingRequestedMode = None
         self._testClient = testClient
+        self._timerLock = threading.Lock()
+        self._pendingMode = None
 
     def run(self, runForever=True):
         self.connect(runForever)
@@ -63,20 +65,46 @@ class HvacController(ClimateMqttClient):
         if not self._initialized:
             self._pendingRequestedMode = requestedMode
             return
-        if self._timer is not None:
-            self._timer.cancel()
-            self._timer = None
+        if requestedMode == self._currentMode:
+            print(f'Current mode "{self._currentMode}" already at requested mode')
+            return
         newMode, self._currentModeExpiration = self._policy.processModeRequest(self._currentMode, self._currentModeExpiration, requestedMode)
+        print(f'Requested mode "{requestedMode}", current mode "{self._currentMode}", got new mode "{newMode}" with expiration {self._currentModeExpiration:.2f}, current time {time.time():.2f}')
         if newMode is not None and newMode != self._currentMode:
             self._currentMode = newMode
             print(f'HVAC controller {self._stationId} mode set to {self._currentMode}')
             self._reportMode()
         if newMode != requestedMode:    # Can't change to the requested mode yet
+            if self._currentModeExpiration <= time.time():
+                print('expiration time not in the future')
+                return
             # Set a timer to try again after the expiration
             # TODO: May need to set the timer just past this time in case the timing is off. Probably not, but maybe
-            self._timer = threading.Timer(self._currentModeExpiration - time.time(), self._setMode, args=(requestedMode,))
-            self._timer.start()
+            self._scheduleReattemptSetMode(requestedMode)
             return
+    
+    def _scheduleReattemptSetMode(self, requestedMode):
+        print('Request to set timer')
+        with self._timerLock:
+            self._pendingMode = requestedMode
+            if self._timer is not None:
+                # A timer is already running
+                print('Not setting new timer: timer already running')
+                return
+            else:
+                timerTime = self._currentModeExpiration - time.time()
+                self._timer = threading.Timer(timerTime, self._setModeFromTimer)
+                print(f'Starting timer with timeout {timerTime:.2f}')
+                self._timer.start()
+
+    def _setModeFromTimer(self):
+        print('Timer fired')
+        with self._timerLock:
+            self._timer.cancel()
+            self._timer = None
+            pendingMode = self._pendingMode
+            self._pendingMode = None
+        self._setMode(pendingMode)
 
     def _reportMode(self):
         self.publish(self._stationId, HVAC_MSG_TYPE_CURRENT_MODE, self._currentMode, qos=1, retain=True)
